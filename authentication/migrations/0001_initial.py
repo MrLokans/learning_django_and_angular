cb2b9e391fb62eca3d0c1ef78c98e546f10a01e4
MrLokans <trikster1911@gmail.com>
2016-01-30 20:29:31 +0300

    Added initial migration.

---
+++ authentication/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('username', models.CharField(max_length=50, unique=True)),
+                ('first_name', models.CharField(max_length=40, blank=True)),
+                ('last_name', models.CharField(max_length=40, blank=True)),
+                ('tagline', models.CharField(max_length=140, blank=True)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
