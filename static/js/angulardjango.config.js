894ec5351d6480b530222fa67037afaf4af960db
MrLokans <trikster1911@gmail.com>
2016-01-31 08:33:13 +0300

    Scripts added to the page.

--- static/js/angulardjango.config.js
+++ static/js/angulardjango.config.js
@@ -2,11 +2,11 @@
     'use strict';
 
     angular.module('angulardjango.config')
-           .confif(config);
+           .config(config);
 
     config.$inject = ['$locationProvider'];
 
-    function $config($locationProvider){
+    function config($locationProvider){
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
     }
