ae06e0e17fc8e27fdee4d1ff22b8530a666edd0d
MrLokans <trikster1911@gmail.com>
2016-01-31 08:36:27 +0300

    Added CSRF support on the client side.

--- static/js/angulardjango.js
+++ static/js/angulardjango.js
@@ -6,5 +6,14 @@
 
     angular.module('angulardjango.config', []);
 
+
+
+    angular.module('angulardjango').run(run);
+
+    // Updating xsrf headers to cope with Django csrf token protection
+    function run($http){
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCoolieName = 'csrftoken';
+    }
 })();
 
