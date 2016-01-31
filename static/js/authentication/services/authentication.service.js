634001cded927a1e55a227b485664f67341d8a51
MrLokans <trikster1911@gmail.com>
2016-01-31 09:00:00 +0300

    Added client-side login method.

--- static/js/authentication/services/authentication.service.js
+++ static/js/authentication/services/authentication.service.js
@@ -8,16 +8,23 @@
 
     function Authentication($cookies, $http){
         var Authentication = {
+            login: login,
             register: register
         };
         return Authentication;
 
-        
+
         function register(email, password, username){
             return $http.post('/api/v1/accounts/', {
                 username: username,
                 password: password,
                 email: email
+            });
+        }
+
+        function login(email, password){
+            return $http.post('/api/v1/auth/login', {
+                email: email, password: password
             });
         }
     }
