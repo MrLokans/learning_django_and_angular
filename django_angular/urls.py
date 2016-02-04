070e11d83d176196e988ce0958e1bc8792d4463e
MrLokans <trikster1911@gmail.com>
2016-02-04 22:03:29 +0300

    Added logout url.

--- django_angular/urls.py
+++ django_angular/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from django_angular.views import IndexView
 
 from rest_framework import routers
-from authentication.views import AccountViewSet, LoginView
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -13,5 +13,6 @@ urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^$', IndexView.as_view(), name="index")
 ]
