7e282655776751946adb60b0c0a3d18b8139efa9
MrLokans <trikster1911@gmail.com>
2016-02-07 15:38:54 +0300

    Added posts controller and directive.

--- static/js/angulardjango.routes.js
+++ static/js/angulardjango.routes.js
@@ -6,17 +6,25 @@
     config.$inject = ['$routeProvider'];
 
     function config($routeProvider){
+
         $routeProvider.when('/register', {
             controller: 'RegisterController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/authentication/register.html'
         }).otherwise('/');
+
         $routeProvider.when('/login', {
             controller: 'LoginController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/authentication/login.html'
         }).otherwise('/');
 
+        $routeProvider
+            .when('/', {
+                controller: 'IndexController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/layout/index.html'
+        });
 
     }
 })();
